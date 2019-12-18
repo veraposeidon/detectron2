162d657f35a3ff9cd596da37904c2b14d8c781e0
veraposeidon <veraposeidon@gmail.com>
2019-12-18 20:47:20 +0800

    针对AL数据集，改进了项目中的dataset.py

--- projects/MultiTask/multitask/dataset.py
+++ projects/MultiTask/multitask/dataset.py
@@ -15,32 +15,35 @@ CLASS_NAMES = ["BuDaoDian", "CaHua", "JiaoWeiLouDi", "JuPi", "LouDi", "PengLiu",
 
 
 # 重写VOC实例加载，因为除了目标检测，还要加载多标签和分割样本
-def load_al_voc_instances(dirname: str, split: str):
+def load_al_voc_instances(dir_name: str, split: str):
     """
     Load Pascal VOC detection annotations to Detectron2 format.
 
     Args:
-        dirname: Contain "Annotations", "ImageSets", "JPEGImages"
+        dir_name: Contain "Annotations", "ImageSets", "JPEGImages"
         split (str): one of "train", "test", "val", "trainval"
     """
-    with PathManager.open(os.path.join(dirname, "ImageSets", "Main", split + ".txt")) as f:
+    with PathManager.open(os.path.join(dir_name, "ImageSets", "Main", split + ".txt")) as f:
         fileids = np.loadtxt(f, dtype=np.str)
 
     dicts = []
     for fileid in fileids:
-        anno_file = os.path.join(dirname, "Annotations", fileid + ".xml")
-        jpeg_file = os.path.join(dirname, "JPEGImages", fileid + ".jpg")
+        anno_file = os.path.join(dir_name, "Annotations", fileid + ".xml")
+        jpeg_file = os.path.join(dir_name, "JPEGImages", fileid + ".jpg")
+        segm_file = os.path.join(dir_name, "SegmentationClassPNG", fileid + ".png")     # 铝材表面分割图
 
         tree = ET.parse(anno_file)
 
         r = {
             "file_name": jpeg_file,
+            "seg_file_name": segm_file,
             "image_id": fileid,
             "height": int(tree.findall("./size/height")[0].text),
             "width": int(tree.findall("./size/width")[0].text),
         }
         instances = []
-
+        multi_labels = []    # 多标签整理
+        multi_label_names = []
         for obj in tree.findall("object"):
             cls = obj.find("name").text
             # We include "difficult" samples in training.
@@ -60,7 +63,14 @@ def load_al_voc_instances(dirname: str, split: str):
             instances.append(
                 {"category_id": CLASS_NAMES.index(cls), "bbox": bbox, "bbox_mode": BoxMode.XYXY_ABS}
             )
+
+            if CLASS_NAMES.index(cls) not in multi_labels:
+                multi_labels.append(CLASS_NAMES.index(cls))
+                multi_label_names.append(cls)
+
         r["annotations"] = instances
+        r["multi_labels"] = multi_labels
+        r["multi_label_names"] = multi_label_names
         dicts.append(r)
     return dicts
 
@@ -68,7 +78,7 @@ def load_al_voc_instances(dirname: str, split: str):
 def register_pascal_voc_al(name, dirname, split):
     DatasetCatalog.register(name, lambda: load_al_voc_instances(dirname, split))
     MetadataCatalog.get(name).set(
-        thing_classes=CLASS_NAMES, dirname=dirname, split=split, year = 2012    # 2012 是因为PASCAL evaluator中有断言2007或2012
+        thing_classes=CLASS_NAMES, dirname=dirname, split=split, year=2012  # 2012 是因为PASCAL evaluator中有断言2007或2012
     )
 
 
