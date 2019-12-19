90c19c001ddcf440a8a1d0b66ee10c5a28e2136f
shen <veraposeidon@gmail.com>
2019-12-19 10:50:32 +0800

    Create Multi Task Project default config

---
+++ projects/MultiTask/multitask/config.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+from detectron2.config import CfgNode as CN
+
+
+def add_multitask_config(cfg):
+    """
+    Add config for multi task project.
+    """
+    _C = cfg
+
+    # 使用多任务模式
+    _C.MODEL.MULTI_TASK_ON = True
+    _C.MODEL.MULTI_TAK = CN()
+    # 开启多标签分类任务
+    _C.MODEL.MULTI_TASK.CLASSIFICATION_ON = True
+    # 开启目标检测任务
+    _C.MODEL.MULTI_TASK.DETECTION_ON = True
+    # 开启分割任务
+    _C.MODEL.MULTI_TASK.SEGMENTATION_ON = True
